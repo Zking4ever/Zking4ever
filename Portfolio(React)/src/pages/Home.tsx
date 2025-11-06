1d931ee9dc8339405661eb924094ad0fa500d6d6
Astawus <yanosastawus@gmail.com>
2025-11-06 20:58:21 +0300

    job titles with animation added

--- Portfolio(React)/src/pages/Home.tsx
+++ Portfolio(React)/src/pages/Home.tsx
@@ -11,8 +11,19 @@ const Home = ({clickHandler}: {clickHandler:(x:string)=>void}) => {
                         <h1>👋</h1>
                     </div>
                     <h3><a href='https://google.com/astawus amsalu'>I'm Astawus Amsalu</a></h3>
-                    <p className="intro"> A passion 
-                        <h2>Full Stack Developer</h2>
+                    <p className="intro"> A passionate 
+                        <div aria-hidden className='jobTitle-container'>
+                            <div className="jobTitles">
+                              <span>Full Stack Developer</span>
+                              <span>Mobile App Developer</span>
+                              <span>Frontend Developer</span>
+                            </div>
+                            <div className="jobTitles">
+                              <span>Full Stack Developer</span>
+                              <span>Mobile App Developer</span>
+                              <span>Frontend Developer</span>
+                            </div>
+                        </div>
                         obssesed in creating interactive and user-friendly websites/applications. 
                     </p>
                     <button onClick={()=>clickHandler('projects')} id='projects'>View My Work</button>
@@ -28,15 +39,18 @@ const Home = ({clickHandler}: {clickHandler:(x:string)=>void}) => {
                         <p className="home-title-desc">Here are all the services that i provide to my clients. I Hope I offer the service that you are looking for. If you do not find something that you are looking for than please send me an email.</p>
                     </div>
                     <div className="work-container">
-                        <div className="work">
+                        <div className="work" data-aos="fade-up"
+     data-aos-duration="3000">
                             <h3>Web Development</h3>
                             <p>I build responsive and dynamic websites that provide an engaging user experience. From landing pages to complex web applications, I create solutions that meet your business needs.</p>
                         </div>
-                        <div className="work">
+                        <div className="work" data-aos="fade-up"
+     data-aos-duration="3000">
                             <h3>UI/UX Design</h3>
                             <p>I design intuitive and visually appealing user interfaces that enhance user experience. My designs are user-centered, ensuring that your audience can easily navigate and interact with your digital products.</p>
                         </div>
-                        <div className="work">
+                        <div className="work" data-aos="fade-up"
+     data-aos-duration="3000">
                             <h3>Consulting</h3>
                             <p>I offer expert advice on web development and digital strategies. Whether you're looking to improve your existing website or plan a new project, I provide insights and recommendations to help you achieve your goals.</p>
                         </div>
