7ee2247f31336992e9e1cd1b0468dfe4ddfbeb97
Astawus <yanosastawus@gmail.com>
2025-11-19 20:11:35 +0300

    Wrote my About content and install react-router-dom

--- Portfolio(React)/src/pages/About.tsx
+++ Portfolio(React)/src/pages/About.tsx
@@ -27,8 +27,20 @@ const About = () => {
             <div className='home'>
                 <section className="welcome-section">
                     <h1>About</h1>
-                    <p> A software developer with a passion for creating interactive and user-friendly applications. 
-                         and get to know more about my work.
+                    <p> 
+                        Do you remember the day you said "Ohh..I'm born for this" for the first time? you might have said it a lot:)
+                         but let me tell my story. My name is Astawus which means 'remeber!' and I do remember:). My tech journey was 
+                         stated with my poor old lovely desktop(lets call her PLO).Everything was normal until PLO goes down 
+                         almost to death which was when i was G-9 student.I reached every possible solution in the internet,wathed a ton 
+                         of videos to figure out PLO's problem. I spoted every components inside PLO and it took me a week to bring her back.
+                         I was so proud of myself and told the crowd what i did. Since then I was the one who fix electronic devices in the neghbourhood.
+                         My curiousity didn't stop with hardware that i stared learning what is really behind the scene. I stared to explore the software world and got introduce
+                         to operating systems and programming languages. This was the moment i got obssesd with programming. I learnt C++,Java, HTML,CSS and Javascript.
+                         As a boy, loved the frontend because of the canva to do on visible things than working on terminals. It was so cool learning web development that i tried 
+                         to build a lot of staffs on PLO. My obssesion with programming kept growing up that I learned different languages expanding the skillkit.
+                         I started working on backend logics mastering writing efficient algorithms. Fast forward... Even though I had the chance and everyone wanted me to join Medicine, I chose Computer Science.
+                         And currently I'm a fullstack developer working on different fields building ground breaking products. There is a lot to come so stay tuned.
+
                     </p>
                 </section>
                 <section className='profile'>
