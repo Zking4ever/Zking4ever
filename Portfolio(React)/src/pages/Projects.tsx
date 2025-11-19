4aa77a1589542ddee5ca73cc2d41b89e4225313a
Astawus <yanosastawus@gmail.com>
2025-11-19 22:10:21 +0300

    added some project data and project card mobile responsiviness bug fixed

--- Portfolio(React)/src/pages/Projects.tsx
+++ Portfolio(React)/src/pages/Projects.tsx
@@ -5,17 +5,25 @@ const projectsList = {
     "projectCatagory":"Web DEVELOPMENT",
     "projects":[
         {
-            title: 'Project One',
-            description: 'This is a brief description of Project One.',
-            link: 'https://github.com/user/project-one',
-            tools: ['React', 'TypeScript', 'CSS']
+            title: 'Easter Wish bord',
+            description: 'An aesthetic chat bord where users can post their wishes and thoughts anonymously. It gave everyone chance to spit out what the have passed,what they felt and much more. It offers a seamless user experience with real-time updates.',
+            link: 'https://easterchat.atwebpages.com',
+            tools: ['HTML', 'Javascript', 'CSS','PHP']
         },
         {
-            title: 'Project Two',
-            description: 'This is a brief description of Project Two.',
-            link: 'https://github.com/user/project-two',
-            tools: ['React', 'TypeScript', 'CSS']
+            title: 'Tic-Tac-Toe Game',
+            description: 'A simple intractive game to play the traditional tic-tac-toe game with support of two players and auto-detection of winner',
+            link: 'https://zking4ever.github.io/Tic-Tac-Toe-Game/',
+            gitlink:'https://github.com/Zking4ever/Tic-Tac-Toe-Game',
+            tools: ['HTML', 'Javascript', 'CSS']
         },
+        {
+            title: 'ODA social media',
+            description: 'A social media platform that allows users to connect, share content, and engage with others. It features user profiles, friend connections, content sharing, and real-time notifications to keep users informed about their network activities.',
+            link: 'https://oda.social-networking.me',
+            tools: ['HTML', 'Javascript', 'CSS','PHP','MySQL']
+        },
+        
         {
             title: 'Project Three',
             description: 'This is a brief description of Project Three. loremdfdjhvbjb j  This is a brief description of Project Three.This is a brief description of Project Three.',
